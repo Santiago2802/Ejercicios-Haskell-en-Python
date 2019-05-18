30493a1fcc0e5814376de49f5dfbb72beabcd2b4
Santiago2802 <50509054+Santiago2802@users.noreply.github.com>
2019-05-18 17:12:32 -0500

    Create igualLista.py
    Comparar si 2 listas son iguales

---
+++ igualLista.py
@@ -0,0 +1,8 @@
+def igualLista(lista1,lista2):
+    if lista1 == lista2:
+        return True
+    return False
+    
+print(igualLista(["Hola","Mundo"],["Mundo","Hola"]))
+
+#Santiago Gómez Almeyda 20161020503
